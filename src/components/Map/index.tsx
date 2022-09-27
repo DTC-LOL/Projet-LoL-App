84acd9e0a46459041921166bdce3fb2b984b1578
Aliseth <charlotte.saidi@outlook.fr>
2022-09-27 12:24:39 +0200

    install axios and test fetching

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,11 +2,34 @@ import React from 'react';
 import styled from 'styled-components';
 import Konva from 'konva';
 import { Stage, Layer, Text } from 'react-konva';
+import axios from 'axios';
 import useBreakpoint from '../../hooks/useBreakpoints';
 
 const Map: React.FC = () => {
 	const {isTablet} = useBreakpoint();
 	console.log('isTablet : ', isTablet);
+
+	// fetch game by uuid
+	axios.get('http://localhost:8000/api/game?uuid=EUW1_6081248019', {
+		headers: {
+			'content-type': 'application/json',
+		}
+	}).then((response) => {
+		console.log(response);
+	}).catch((error) => {
+		console.log(error);
+	});
+
+	// fetch player by name
+	axios.get('http://localhost:8000/api/player?name=qrab', {
+		headers: {
+			'content-type': 'application/json',
+		}
+	}).then((response) => {
+		console.log(response);
+	}).catch((error) => {
+		console.log(error.message);
+	});
 	
 	return (
 		<Container width={isTablet ? 600 : 300} height={isTablet ? 600 : 300}>
