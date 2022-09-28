4819169ba9295545715a3697b7808c8de2b8aef5
Lucas Barq <lucasbarq@free.fr>
2022-09-28 11:59:24 +0200

    ✨ Add Filters on map
    Co-authored-by: Florian Galvani <FlorianGalvani@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './components/Map';
+import Filters from './components/Filters';
 
 function App() {
 	return (
 		<div className="App">
 			<Map/>
+			<Filters />
 		</div>
 	);
 }
